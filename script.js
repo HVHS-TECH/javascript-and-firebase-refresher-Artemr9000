3323ed4df5aca12ab41815d0a2481d251c8ced12
Artem Rakhimov <22155ar@hvhs.school.nz>
2026-02-04 12:20:02 +1300

    step two and 3 complete

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ console.log("Hello world")
 document.getElementById("welcomeMessage").innerHTML = "You've connected to the JavaScript!";
 
 //second step\\ 
-document.addEventListener('DOMContentLoaded', () => {
 function myFunction() {
-  document.getElementById("demo").innerHTML = "Hello World";
+  document.getElementById("welcomeMessage").innerHTML = "Hello World";
 }
-});
+
+//third step\\ 
+function thirdstep() {
+  var x = document.getElementById("myText").value;
+  document.getElementById("demo").innerHTML = x;
+}
