96aa017e7faaea8d5292b4612a82cd5752a5fb52
Artem Rakhimov <22155ar@hvhs.school.nz>
2026-02-04 12:02:07 +1300

    on to step 2

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-var messageSpace = getElementbyId("welcomeMessage");
-messageSpace.innerHTML = "You've connected to the JavaScript!";
+//first step\\ 
+console.log("Hello world")
+document.getElementById("welcomeMessage").innerHTML = "You've connected to the JavaScript!";
+
+//second step\\ 
+document.addEventListener('DOMContentLoaded', () => {
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World";
+}
+});
