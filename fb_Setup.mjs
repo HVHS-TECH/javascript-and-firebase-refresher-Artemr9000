230408730373a96d99afcac63d54088630070bbb
Artem Rakhimov <22155ar@hvhs.school.nz>
2026-02-05 13:29:10 +1300

    set up fire base

---
+++ fb_Setup.mjs
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCJJ8-ZerBC53qhRMzinJiPty2vk9tSsKc",
+  authDomain: "artem-rakhimov-13comp-c7551.firebaseapp.com",
+  databaseURL: "https://artem-rakhimov-13comp-c7551-default-rtdb.firebaseio.com",
+  projectId: "artem-rakhimov-13comp-c7551",
+  storageBucket: "artem-rakhimov-13comp-c7551.firebasestorage.app",
+  messagingSenderId: "902104968011",
+  appId: "1:902104968011:web:2cf4178eeb8c04428796ce"
+};
